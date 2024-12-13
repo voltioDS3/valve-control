faadfd65572c0bd2c656d431c67bd2678caaca74
voltioDS3 <voltio.dibujos@gmail.com>
2024-12-13 01:40:51 -0400

    added NTP server and RTC reads, also added functionallity to work with the valve object

--- src/main.h
+++ src/main.h
@@ -6,10 +6,19 @@
 #include <ESP32Time.h>
 #include <TaskScheduler.h>
 
+#include <time.h>
+
 #include "wifi_utils.h"
 #include "dashboard.h"
 #include "pins.h"
 #include "valve.h"
 
 
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = -10800;
+const int   daylightOffset_sec = 3600;
+unsigned long lastSyncTime = 0; // Última sincronización con NTP
+const unsigned long syncInterval = 24 * 60 * 60 * 1000; // Cada 24 horas
+const unsigned long valve_check_interval= 5*60*1000; // chechea cad 5 minutos
+unsigned long last_valve_check=0;
 #endif
